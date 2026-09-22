5409c1e8152378268506faa184ec48d2c918c3a6
yechwood <kedemwoodlake@gmail.com>
2026-09-22 23:24:02 +0800

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.wazeinthandler"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.wazeinthandler"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
